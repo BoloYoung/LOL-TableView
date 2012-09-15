e45624c03a5721e89b95e59322f7a62d36185479
SZ <magicstudio@MagicStudiomatoMac.local>
2012-09-15 14:16:09 +0800

    Initial Commit

---
+++ LOL/LOLAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  LOLAppDelegate.h
+//  LOL
+//
+//  Created by MagicStudio on 12-9-15.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LOLAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ LOL/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  LOL
+//
+//  Created by MagicStudio on 12-9-15.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
